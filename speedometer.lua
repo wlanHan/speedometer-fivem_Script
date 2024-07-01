4bfe16a2b1e1464d1e40215dc671a82ef63fabb4
Efe Han Oz <159789584+wlanHan@users.noreply.github.com>
2024-07-01 18:30:50 +0300

    Citizen +

--- speedometer.lua
+++ speedometer.lua
@@ -1,6 +1,17 @@
 function text(content)
     SetTextFont(1)
     SetTextProportional(0)
-    SetTextScale(1.9,1.9)
+    SetTextScale(1.9, 1.9)
     SetTextEntry("STRING")
     AddTextComponentString(content)
+    DrawText(0.9, 0.7)
+end
+
+Citizen.CreateThread(function()
+
+    while true do
+        Citizen.Wait(2)
+        local speed = GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))
+    end
+
+end)
